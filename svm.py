daf540cd22ec072d52b0cc4cb5a30409b017e9cc
RINQERDESKTOP\Tomoki <furukawa12122003@gmail.com>
2024-11-12 04:55:06 +0900

    SVMモデルのトレーニングスクリプトを改善。特徴量の選択を更新し、標準化プロセスの説明を追加。評価結果の表示形式を変更し、データ読み込み関数のドキュメントを強化。

--- svm.py
+++ svm.py
@@ -6,6 +6,7 @@ from traffic_attributes import TrafficAttr
 
 
 SAMPLED_PATH = './201501_sampled.txt'   # SVMのトレーニングに使用するサンプリングデータ (2015年1月)
+# SAMPLED_PATH = './201501_15-31_sampled.txt'   # SVMのトレーニングに使用するサンプリングデータ (2015年1月15日から31日まで)
 # TEST_PATH = './201502_sampled.txt'    # SVMのテストに使用するサンプリングデータ (2015年2月)
 TEST_PATH = './20150201_sampled.txt'    # SVMのテストに使用するサンプリングデータ (2015年2月1日)
 
@@ -27,12 +28,12 @@ NUMERIC_FEATURES = [
 
 # 使用するカテゴリカルデータの特徴量
 CATEGORICAL_FEATURES = [
-    # TrafficAttr.IDS_DETECTION.name,
-    # TrafficAttr.MALWARE_DETECTION.name,
-    # TrafficAttr.ASHULA_DETECTION.name,
-    TrafficAttr.SERVICE.name,
-    TrafficAttr.FLAG.name,
-    TrafficAttr.PROTOCOL.name,
+    TrafficAttr.IDS_DETECTION.name,
+    TrafficAttr.MALWARE_DETECTION.name,
+    TrafficAttr.ASHULA_DETECTION.name,
+    # TrafficAttr.SERVICE.name,
+    # TrafficAttr.FLAG.name,
+    # TrafficAttr.PROTOCOL.name,
 ]
 
 
@@ -42,27 +43,48 @@ def main():
     X_test, y_test = load_and_preprocess_data(TEST_PATH)
 
     # トレーニングデータとテストデータの特徴量を一致させる
+    # すべての特徴量が両方のデータセットに存在するようにし、不足している特徴量には0を埋める
     X_train, X_test = X_train.align(X_test, join='outer', axis=1, fill_value=0)
 
-    # データの標準化
+    # トレーニングデータとテストデータを標準化する
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
     # SVMモデルの定義と学習
+    # RBFカーネルを使用し、ハイパーパラメータCとgammaを設定してモデルをトレーニングデータで学習させる
+    # - kernel: 'rbf'はガウシアンカーネル（RBFカーネル）を使用することを指定
+    # - C: 誤分類をどれだけ許容するかを制御するパラメータ。大きい値にすると誤分類を減らすが、過学習のリスクが増える
+    # - gamma: RBFカーネルの幅を制御するパラメータ。大きい値にするとモデルがより複雑になり、過学習のリスクが増える
+    # - random_state: 結果の再現性を確保するための乱数シード
+    # - max_iter: 最大反復回数。収束しない場合の停止条件
     model = SVC(kernel='rbf', C=100, gamma=0.1, random_state=42, max_iter=5000)
     model.fit(X_train, y_train)
 
     # テストデータでの評価
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f'Accuracy: {accuracy:.4f}')
-    print(classification_report(y_test, y_pred))
+    y_pred = model.predict(X_test)  # テストデータを使って予測を行う
+    accuracy = accuracy_score(y_test, y_pred)   # 予測の正確さを計算
+    print(f'Accuracy: {accuracy:.5f}')  # 精度を表示
+    print(classification_report(y_test, y_pred))    # 適合率、再現率、F1スコアを表示
 
 
 def load_and_preprocess_data(file_path):
     '''
-    データを読み
+    指定されたファイルパスからデータを読み込み、前処理を行う関数。
+
+    前処理の手順:
+    1. データを読み込む。
+    2. 各ラベルのサンプル数を表示する。
+    3. 必要な特徴量のみを選択する。
+    4. カテゴリカルデータをダミー変数に変換する。
+    5. 特徴量とラベルに分割する。
+
+    引数:
+    file_path (str): データファイルのパス。
+
+    戻り値:
+    X (DataFrame): 特徴量データ。
+    y (Series): ラベルデータ。
     '''
     data = pd.read_csv(file_path, delimiter='\t')
 
